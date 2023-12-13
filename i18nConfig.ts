b006aa997d468a81423706b427cf990b085ad77a
Williams Mejias <willamsmejias1049@gmail.com>
2023-12-12 23:43:25 -0400

    test

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -24,7 +24,7 @@ const resources = {
 };
 
 i18n.use(initReactI18next).init({
-  resources,
+  resources: resources || {},
   lng: "en",
   interpolation: {
     escapeValue: false,
--- src/components/Home-page/HomePage.test.tsx
+++ src/components/Home-page/HomePage.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import HomePage from "./HomePage";
 import "../../../i18nConfig";
+
 test("renders HomePage component", () => {
   render(<HomePage />);
 });
@@ -16,16 +17,8 @@ test("renders ColumnA and ColumnB components", () => {
   expect(columnBElements.length).toBeGreaterThan(0);
 });
 
-// test("renders ColumnB with rickMorty prop set to true", () => {
-//   const { getByTestId } = render(<HomePage />);
-//   const columnBWithProps = getByTestId("columnBWithProps");
-//   expect(columnBWithProps).toBeInTheDocument();
-//   expect(columnBWithProps).toHaveAttribute("rickMorty", "true");
-// });
-
-// test("renders ColumnA with rickMorty prop set to true", () => {
-//   const { getByTestId } = render(<HomePage />);
-//   const columnAWithProps = getByTestId("columnAWithProps");
-//   expect(columnAWithProps).toBeInTheDocument();
-//   expect(columnAWithProps).toHaveAttribute("rickMorty", "true");
-// });
+test("renders ColumnB with rickMorty prop set to true", () => {
+  const { getByTestId } = render(<HomePage />);
+  const columnBWithProps = getByTestId("columnBWithProps");
+  expect(columnBWithProps).toBeInTheDocument();
+});
